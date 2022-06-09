aadad0570af5b1ff720cf527ec9185d3bd4ca56e
diogoaraujo <diogorjbr@gmail.com>
2022-06-09 15:57:32 -0300

    separa lista e faz input

--- count-set-ngram.py
+++ count-set-ngram.py
@@ -5,18 +5,33 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import json
+import ast
 
 #1. Pegar conteúdo HTML a partir da URL
 url = "https://books.google.com/ngrams#"
 
 option = Options()
 option.headless = True
-#driver = webdriver.Firefox(executable_path=r'C:\usr\local\bin\geckodriver.exe')
 driver = webdriver.Firefox()
+# codigo executa em background 
+# driver = webdriver.Firefox(options=option)
 driver.get(url)
 
-time.sleep(10)
 
+#import json and convert to list
+with open('categorias.json') as json_file:
+    data = json.load(json_file)
+print(data)
+
+source = driver.find_element_by_xpath(
+    "//input[@type='text']")
+source.clear()
+source = driver.find_element_by_xpath(
+    "//input[@type='text']")
+source.send_keys(data)
+source.send_keys(Keys.ENTER)
+
+time.sleep(10)
 
 
 #2. Parsear o conteúdo HTML - BeautfulSoup
--- separa-e-conta-1.py
+++ separa-e-conta-1.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from pandas import *
+import json
+import re
 
 xls = ExcelFile('22-06-01 eng-words-change-1800-2019code-AS-FINAL.xlsx')
 df = xls.parse(xls.sheet_names[0])
@@ -11,9 +13,20 @@ xls2 = pd.read_excel(xls, index_col=None, header=None)
 Function_system = df['Function system'].tolist()
 Function_system = list(dict.fromkeys(Function_system))
 
-print(Function_system)
+# print(Function_system)
+categorias_0 = json.dumps(Function_system)
+categorias_1 = re.sub('"','',categorias_0)
+categorias = categorias_1.replace('[', '').replace(']', '')
+print(categorias)
+#print(type(categorias_1))
+
+
+
+#save json to file
+with open('categorias.json', 'w') as jsonfile:
+    json.dump(categorias, jsonfile)
 
 contando = df.groupby(['Function system'])['word'].count()
 
-print(contando)
+# print(contando)
 
